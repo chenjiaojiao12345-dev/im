121dd29a28a7ffae366d8b8c12f989d285c6f7a1
leili <hafu@hafu.com>
2026-01-31 12:00:56 +0800

    调整

--- config/context.go
+++ config/context.go
@@ -191,9 +191,9 @@ func (c *Context) checkAdminPermission(ctx *wkhttp.Context) {
 
 // isIPInAdminWhitelist 判断IP是否在后台白名单中
 func (m *Context) isIPInAdminWhitelist(ip string) (bool, error) {
-	if ip == "127.0.0.1" || ip == "0.0.0.0" {
-		return true, nil
-	}
+	//if ip == "127.0.0.1" || ip == "0.0.0.0" {
+	//	return true, nil
+	//}
 
 	var cnt int64
 
